dedefdea2f1420f7afdfee54d68937c70f9c2e54
Phumsan <52192362+Phumsan@users.noreply.github.com>
2019-09-03 17:38:58 +0700

    Add files via upload

---
+++ homeworkfunction.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+float update_balance(string command,float dollars,float &balance);
+int main()
+{
+	float dollars;
+	float balance = 10000;
+	string command;
+	cout << "Exit with 0\n";
+	cout << fixed;
+    cout << setprecision(2);
+	cout << "Your balance : " << balance << endl; 
+	do{
+		cout << "Input command (1 or withdraw, 2 deposit ) : ";
+		cin >> command;
+		if(command == "1" || command == "2")
+		{
+			cout << "Input amount : ";
+		    cin >> dollars;
+		    cout << "Your balance : " << update_balance(command,dollars,balance) << endl;
+		}
+	}while(command != "0");
+
+}
+float update_balance(string command,float dollars,float &balance)
+{
+	if(command == "1")
+	{
+		return(balance = balance-dollars);
+	}
+	else if(command == "2")
+	{
+		return(balance = balance+dollars);
+	}
+}
+
+
+
+
